aa3fb85bfa6c9e15fb8fdb1b1de6f5dd277a2465
nobyungseo <nobyungsea@naver.com>
2023-06-21 23:03:28 +0900

    Spring 4강 과제 퀴즈2번 / sql비번 바꾸기!!!!

--- src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
+++ src/main/java/com/quiz/lesson04/Lesson04Quiz02Controller.java
@@ -1,10 +1,46 @@
 package com.quiz.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.quiz.lesson04.bo.RealtorBO;
+import com.quiz.lesson04.domain.Realtor;
 
 @RequestMapping("/lesson04/quiz02")
 @Controller
 public class Lesson04Quiz02Controller {
+	
+	@Autowired
+	private RealtorBO realtorBO;
+	
+//	http://localhost:8080/lesson04/quiz02/add_realtor_view
+	@GetMapping("/add_realtor_view")
+	public String addRealtorView() {
+		
+		return "lesson04/addRealtor";
+	}
+	
+	
+//	http://localhost:8080/lesson04/quiz02/add_realtor
+	@PostMapping("/add_realtor")
+	public String addRealtor(
+			@ModelAttribute Realtor realtor,
+			Model model
+			) {
+		
+		// insert db
+		realtorBO.addRealtor(realtor);
+		
+		// select db
+		Realtor latestRealtor = realtorBO.getRealtorById(realtor.getId());
+		model.addAttribute("result", latestRealtor);
+		
+		return "lesson04/afterAddRealtor";
+	}
 	
 }
